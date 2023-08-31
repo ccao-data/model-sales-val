1d562d83a9c275c0ab35b960e33e6b258e4f2fd6
Michael Wagner <miwagne@ccao-datals.ccao.local>
2023-08-31 15:06:16 +0000

    Fix comments on rolling window

--- glue/sales_val_flagging.py
+++ glue/sales_val_flagging.py
@@ -51,10 +51,10 @@ def add_rolling_window(df, num_months):
     WARNING: num_months cannot go over 12 or this function breaks
 
     Inputs:
-        df: dataframe of sales that we need to flag, data should be 11 months back
+        df: dataframe of sales that we need to flag, data should be N months back
             from the earliest unflagged sale in order for the rolling window logic to work
     Outputs:
-        df: dataframe that has exploded each observation into 12 observations with a 12 distinct
+        df: dataframe that has exploded each observation into N observations with a 12 distinct
             rolling window columns
     """
     max_date = df["meta_sale_date"].max()
