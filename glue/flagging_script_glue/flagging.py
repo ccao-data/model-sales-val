4e5cfa6b62a97eee0c440b82077190cc40574da9
Michael Wagner <wagnerleemichael@gmail.com>
2023-10-31 16:05:40 +0000

    Remove log change to z_normalize_groupby

--- glue/flagging_script_glue/flagging.py
+++ glue/flagging_script_glue/flagging.py
@@ -711,8 +711,8 @@ def z_normalize_groupby(s: pd.Series):
         z_normalized series grouped by class and township
         that is then stitched into complete column by pandas
     """
-    log_values = np.log(s)  # Take the log of the series values before normalization
-    return zscore(log_values, nan_policy="omit")
+
+    return zscore(s, nan_policy="omit")
 
 
 def outlier_type(df: pd.DataFrame, condos: bool) -> pd.DataFrame:
