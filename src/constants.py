0899f6c06968d596d6eb12d4c09bf0d5f3018428
Michael Wagner <wagnerleemichael@gmail.com>
2026-06-11 14:55:07 -0500

    Stub the dev table

--- src/constants.py
+++ src/constants.py
@@ -7,7 +7,7 @@ import yaml
 LOCATION_NEIGHBORHOOD_GROUP_TABLE = "location.neighborhood_group"
 DEFAULT_CARD_RES_CHAR_TABLE = "default.vw_card_res_char"
 DEFAULT_VW_PIN_CONDO_CHAR_TABLE = "default.vw_pin_condo_char"
-DEFAULT_VW_PIN_SALE_TABLE = "default.vw_pin_sale"
+DEFAULT_VW_PIN_SALE_TABLE = ""
 DEFAULT_VW_PIN_UNIVERSE_TABLE = "default.vw_pin_universe"
 
 with open(os.path.join("src", "inputs.yaml"), "r") as stream:
